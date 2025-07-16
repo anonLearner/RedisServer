cc6eca3b253868d4dd144bac1c696dca450868c2
anonLearner <learneranon@gmail.com>
2025-07-16 06:25:48 +0530

    using threading to handle multiple clients

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import threading
+
 import socket  # noqa: F401
 
 
@@ -8,11 +10,21 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    # conn.sendall(b"+PONG\r\n")
+    
+    while True:
+        conn, addr = server_socket.accept()  # wait for client
+        print(f"Accepted connection from {addr}")
+        # Handle each connection in a new thread
 
-    while conn.recv(1024):
-        conn.sendall(b"+PONG\r\n")
+        def handle_client(client_conn):
+            while True:
+                data = client_conn.recv(1024)
+                if not data:
+                    break
+                client_conn.sendall(b"+PONG\r\n")
+            client_conn.close()
+
+        threading.Thread(target=handle_client, args=(conn,), daemon=True).start()
 
 if __name__ == "__main__":
     main()
