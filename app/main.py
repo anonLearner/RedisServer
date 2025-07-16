4f21f03b22d463407f956ef549b655999b7e2a7a
anonLearner <learneranon@gmail.com>
2025-07-16 06:19:57 +0530

    uncommented code

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    conn.sendall(b"+PONG\r\n")
+    # conn.sendall(b"+PONG\r\n")
 
     while conn.recv(1024):
         conn.sendall(b"+PONG\r\n")
