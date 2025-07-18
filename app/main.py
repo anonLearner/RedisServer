88c48fdd1b19686a77db99275333b85e3aa22446
anonLearner <learneranon@gmail.com>
2025-07-18 07:49:02 +0530

    fixed error related to replica config

--- app/main.py
+++ app/main.py
@@ -260,12 +260,12 @@ def send_command(client_conn, response, replica):
     else:
         resp = format_resp("Error: Unknown command")
 
-    if not replica:
+    if not replica and command!= "psync":
         client_conn.sendall(resp.encode('utf-8'))
-        if command == "psync":
-            empty_rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-            client_conn.sendall(b"$" + str(len(bytes.fromhex(empty_rdb_hex))).encode('utf-8') + b"\r\n" + bytes.fromhex(empty_rdb_hex))
-  
+    if command == "psync":
+        empty_rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+        client_conn.sendall(b"$" + str(len(bytes.fromhex(empty_rdb_hex))).encode('utf-8') + b"\r\n" + bytes.fromhex(empty_rdb_hex))
+
 
 def handle_client(client_conn, replica=False):
     buffer = ""
